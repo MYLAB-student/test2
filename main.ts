e63d65e6fcd0a8ec0d610b0401bb2dd3b3d13a56
MYLAB-student <mylab.student99@gmail.com>
2024-08-27 09:45:57 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    SuperBit.Servo(SuperBit.enServo.S1, 30)
+})
+input.onButtonPressed(Button.B, function () {
+    SuperBit.Servo(SuperBit.enServo.S1, 180)
+})
+basic.forever(function () {
+	
+})
